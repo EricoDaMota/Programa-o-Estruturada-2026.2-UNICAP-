48013ec5e13879b7469826f774b1d123648a5394
EricoDaMota <eriquinhofreire@gmail.com>
2026-08-25 23:12:41 +0000

    atv_01.c

--- VetorMedia/atv_01.c
+++ VetorMedia/atv_01.c
@@ -2,35 +2,45 @@
 
 #define QTD_ALUNOS 30
 
-void digitaNota(int n1[], int n2[], int n);
-void calculaMediaPorAluno(int n1[], int n2[], double med[], int n);
-double calculaMediaTurma(double med[], int n);
+void digitaNota(double n1[], double n2[], int n);
+void calculaMediaPorAluno(double n1[], double n2[], double med[], int n);
+double calculaMediaTurma(double medPA[], int n);
 void imprimeMediaTurma(double medPA[], int n);
-void imprimeNotaAcimaMedia(int n1[], int n2[], double med[], int n);
+void imprimeNotaAcimaMedia(double n1[], double n2[], double med[], int n);
 
 int main(){
-    int nota1[QTD_ALUNOS];
-    int nota2[QTD_ALUNOS];
+    double nota1[QTD_ALUNOS];
+    double nota2[QTD_ALUNOS];
     double media[QTD_ALUNOS];
     digitaNota(nota1, nota2, QTD_ALUNOS);
     calculaMediaPorAluno(nota1, nota2, media, QTD_ALUNOS);
-    calculaMediaTurma(media, QTD_ALUNOS);
     imprimeMediaTurma(media, QTD_ALUNOS);
     imprimeNotaAcimaMedia(nota1, nota2, media, QTD_ALUNOS);
     
     return 0;
 }
-void digitaNota(int n1[], int n2[], int n){
+void digitaNota(double n1[], double n2[], int n){
     for (int i = 0 ; i < n ; i += 1) {
-        printf("Digite a nota1 do aluno(%d/30): ", i + 1);
-        scanf("%d", &n1[i]);
-        printf("Digite a nota2 do aluno(%d/30): ", i + 1);
-        scanf("%d", &n2[i]);
+        printf("\nDigite a nota1 do aluno(%d/%d): ", i + 1, n);
+        scanf("%lf", &n1[i]);
+        while (n1[i] > 10 || n1[i] < 0) {
+            printf("\n(0 <= Nota <= 10)!");
+            printf("\nDigite a nota1 do aluno(%d/%d): ", i + 1, n);
+            scanf("%lf", &n1[i]);
+        }
+        printf("\nDigite a nota2 do aluno(%d/%d): ", i + 1, n);
+        scanf("%lf", &n2[i]);
+        while (n2[i] > 10 || n2[i] < 0) {
+            printf("(0 <= Nota <= 10)!");
+            printf("\nDigite a nota2 do aluno(%d/%d): ", i + 1, n);
+            scanf("%lf", &n2[i]);
+        }
+
     }
 }
-void calculaMediaPorAluno(int n1[], int n2[], double medPA[], int n) {
+void calculaMediaPorAluno(double n1[], double n2[], double medPA[], int n) {
     for (int i = 0 ; i < n ; i += 1) {
-        medPA[i] = ((double) n1[i] * 2 + (double) n2[i] * 3) / 5.0;
+        medPA[i] = (n1[i] * 2 + n2[i] * 3) / 5.0;
     }
 }
 
@@ -44,14 +54,14 @@ double calculaMediaTurma(double medPA[], int n) {
 }
 
 void imprimeMediaTurma(double medPA[], int n) {
-    printf("A média da turma é: %f\n", calculaMediaTurma(medPA, n));
+    printf("A média da turma é: %.1f\n", calculaMediaTurma(medPA, n));
 }
 
-void imprimeNotaAcimaMedia(int n1[], int n2[], double med[], int n){
+void imprimeNotaAcimaMedia(double n1[], double n2[], double med[], int n){
     double medT = calculaMediaTurma(med, n);
     for (int i = 0 ; i < n ; i += 1) {
         if (medT < med[i]) {
-            printf("\nAluno(%d/30):\nnota1 = %d\nnota2 = %d\nMedia = %f\n", i + 1, n1[i], n2[i], med[i]);
+            printf("\nAluno(%d/%d):\nnota1 = %.1f\nnota2 = %.1f\nMedia = %.1f\n", i + 1, n,  n1[i],  n2[i], med[i]);
         }
     }
 }
