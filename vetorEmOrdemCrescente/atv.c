8c92e3189f945dac4b869122d2583b3b97ead32f
EricoDaMota <eriquinhofreire@gmail.com>
2026-08-27 12:00:52 +0000

    atv.c

---
+++ vetorEmOrdemCrescente/atv.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define QTD_ELEMENTOS 5
+
+int main() {
+    
+}
+
+void digitaVetor(v[], n) {
+    for (int i = 0 ; i < n ; i += 1){
+        printf("Digite um valor: ");
+        scanf("%d", v[i]);
+    }
+}
+
+void definirMax(v[], n) {
+    maior = v[0];
+    for (int i = 0 ; i < n ; i += 1){
+        if (maior < v[i]) {
+            maior = v[i];
+        }
+    }
+}
+
+void definirOrdem (v[], n) {
+    digitaVetor(v[] , n);
+    while 
+} 
