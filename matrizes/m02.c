1f5144076bc0c85b3f5c8859d9b284e728f3d3a1
EricoDaMota <eriquinhofreire@gmail.com>
2026-09-17 11:57:18 +0000

    matriz, somatorio, zerar acima, zerar abaixo...

--- matrizes/m02.c
+++ matrizes/m02.c
@@ -8,10 +8,15 @@ void imprimirMatriz(int m[][QTD_COLUNAS], int lins, int coluns);
 void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int coluns);
 
 int main() {
+    int iMaior, jMaior;
     int m[QTD_LINHAS][QTD_COLUNAS];
     preencherMatriz(m, QTD_LINHAS, QTD_COLUNAS);
+    printf("\nImpressão Matriz M\n");
     imprimirMatriz(m, QTD_LINHAS, QTD_COLUNAS);
     imprimirMaiorValorMatriz(m, QTD_LINHAS, QTD_COLUNAS);
+    maiorValorMatriz()
+    printf("O maior valor é %d e está na posição (%d, %d)\n",
+    matriz[iMaior[jMaior], iMaior + 1, jMaior + 1]);
     return 0;
 }
 
@@ -43,4 +48,70 @@ void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int coluns){
         }
 
     }
+}
+
+int buscarMatriz (int m[][QTD_COLUNAS], int lins, int coluns, int x, int *posI, int *posJ) {
+    for (int i = 0 ; i < lins ; i += 1) {
+        for(int j = 0 ; j < coluns ; j += 1) {
+            if (m[i][j] == x) {
+                *posI = i;
+                *posJ = j;
+                return 1;
+            }
+        }
+    }
+    *posI = -1;
+    *posJ = -1;
+    return 0;
+}
+
+void somatorioPorLinha(int m[][QTD_COLUNAS], int lins, int coluns, int soma[]) {
+    for (int i = 0 ; i < lins ; i += 1) {
+        soma[i] = 0;
+        for (int i = 0 ; i < lins ; i += 1) {
+            for (int j = 0 ; j < coluns ; j += 1){
+                soma[i] += m[i][j];
+            }
+            
+        }
+    }
+}
+
+void somatorioPorColuna(int m[][QTD_COLUNAS], int lins, int coluns, int soma[]) {
+    for (int j = 0 ; j < coluns ; j += 1) {
+        soma[j] = 0;
+        for (int i = 0 ; i < coluns ; i += 1) {
+            for (int i = 0 ; i < lins ; i += 1){
+                soma[j] += m[i][j];
+            }
+            
+        }
+    }
+}
+
+//void zerarAbaixoDiagonalPrincipal (int m[][QTD_COLUNAS], int lins, int coluns){
+//    if (lins > coluns) {
+//        for (int i = 0 ; i < QTD_LINHAS ; i += 1) {
+//            for (int j = 0 ; j < QTD_COLUNAS ; j += 1) {
+//                if (i > j) {
+//                    m[i][j] = 0;
+//                }
+//            }
+//        }
+//    }
+//}
+
+void zerarAbaixoDiagonalPrincipal (int m[][QTD_COLUNAS], int lins, int coluns){
+    if (lins > coluns) {
+        for (int i = 1 ; i < lins ; i += 1) {
+            for (int j = 0 ; j < i ; j += 1) {
+                m[i][j] = 0;
+                
+            }
+        }
+    }
+}
+
+void zerarAcimaDiagonalPrincipal (int m[][QTD_COLUNAS], int lins, int coluns){
+    
 }
