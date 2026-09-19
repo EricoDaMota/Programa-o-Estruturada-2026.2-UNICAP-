808a5f8bd24603ca3470cd73c460bcbec1b76319
EricoDaMota <eriquinhofreire@gmail.com>
2026-09-19 22:48:47 +0000

    prova q1

---
+++ provasAntigas/q1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define TAM 5
+
+void preencherVetor(int v[], int n);
+void imprimirVetor(int v[], int n);
+void ordenarBubbleSort(int v[], int n);
+void imprimirMaiorImpar(int v[], int n);
+void criarParesImpares(int v[], int n);
+
+int main() {
+    int numeros[TAM];
+    preencherVetor(numeros, TAM);
+    imprimirVetor(numeros, TAM);
+    ordenarBubbleSort(numeros, TAM);
+    imprimirVetor(numeros, TAM);
+    imprimirMaiorImpar(numeros, TAM);
+    return 0;
+}
+
+void preencherVetor(int v[], int n) {
+    for (int i = 0 ; i < n ; i += 1) {
+        printf("Digite um valor(%d/%d): ", i + 1, n);
+        scanf("%d", &v[i]);
+    }
+}
+
+void imprimirVetor(int v[], int n) {
+    printf("{");
+    for (int i = 0 ; i < n ; i += 1) {
+        printf("%d", v[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("}\n");
+}
+
+void ordenarBubbleSort(int v[], int n) {
+    int aux, limite, trocou, i;
+    limite = n - 1;
+    trocou = 1;
+    while (trocou && limite > 0) {
+        trocou = 0;
+        i = 0;
+        while(i < limite) {
+            if (v[i] > v[i + 1]){
+                aux = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = aux;
+                trocou = 1;
+            }
+            i++;
+        }
+        limite--;
+    }
+}
